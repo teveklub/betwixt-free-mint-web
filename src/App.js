bf50476cc6b432bd8340a0f6941b6e4c5e376c98
Szatmari Csongi <szatmari.csongi@gmail.com>
2022-10-03 11:28:22 +0300

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Home Page</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './index.css';
+import App from './App';
+import { render } from 'react-dom';
+
+const root = document.getElementById("root");
+render(<App />, root);
+
